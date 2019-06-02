e5ad910bb270ad1eb37e1482e9be89a96bcc8014
Pawday <pawday@mail.ru>
2019-06-02 18:05:04 +0300

    Add tools: ConsoleColors

--- src/main/java/per/pawday/httpserver/main/Main.java
+++ src/main/java/per/pawday/httpserver/main/Main.java
@@ -1,10 +1,19 @@
 package per.pawday.httpserver.main;
 
 
+import java.io.File;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        File routerFile = new File("router.json");
+        if ( !routerFile.exists() )
+        {
+            if (! routerFile.canWrite())
+            {
 
+            }
+        }
     }
 }
