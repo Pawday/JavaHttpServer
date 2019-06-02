25e035b501ae2a89a3ba007cf1b229265c544472
Pawday <pawday@mail.ru>
2019-06-02 18:38:11 +0300

    Resources, router.json file, update ConsoleColors

--- src/main/java/per/pawday/httpserver/main/Main.java
+++ src/main/java/per/pawday/httpserver/main/Main.java
@@ -1,7 +1,11 @@
 package per.pawday.httpserver.main;
 
 
-import java.io.File;
+import per.pawday.httpserver.tools.console.ConsoleColors;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class Main
 {
@@ -10,9 +14,32 @@ public class Main
         File routerFile = new File("router.json");
         if ( !routerFile.exists() )
         {
-            if (! routerFile.canWrite())
+            if (routerFile.canWrite() && routerFile.canRead())
             {
+                System.out.print(ConsoleColors.RED);
+                System.out.println("Permission denied: no permission to create and read file: \"router.json.\"");
+                System.out.print(ConsoleColors.RESET);
+                System.exit(-1);
+            }
+            else
+            {
+                try(InputStreamReader stream = new InputStreamReader(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream("examples/router.json")), StandardCharsets.UTF_8);FileOutputStream fileOutputStream = new FileOutputStream(routerFile))
+                {
+                    routerFile.createNewFile();
 
+                    int b;
+                    while ((b = stream.read())!= -1)
+                    {
+                        fileOutputStream.write((char)b);
+                    }
+                }
+                catch (IOException e)
+                {
+                    System.out.print(ConsoleColors.RED);
+                    System.out.println("Error: error creating file \"router.json.\"");
+                    System.out.print(ConsoleColors.RESET);
+                    System.exit(-1);
+                }
             }
         }
     }
