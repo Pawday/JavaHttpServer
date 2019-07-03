04a36edbd1c8a23fc4c6acc2be2e2b857a31e840
Pawday <pawday@mail.ru>
2019-07-03 20:16:09 +0300

    HttpBody class: new parameters

--- src/main/java/org/kondle/httpserver/http/components/body/HttpBody.java
+++ src/main/java/org/kondle/httpserver/http/components/body/HttpBody.java
@@ -1,6 +1,21 @@
 package org.kondle.httpserver.http.components.body;
 
+import java.io.InputStream;
+
 public class HttpBody
 {
+
+
+
+    private boolean hasSize;
+    private boolean isStream()
+    {
+        return this.stream == null;
+    }
+
+    private long size;
+
+    private InputStream stream;
+
 
 }
