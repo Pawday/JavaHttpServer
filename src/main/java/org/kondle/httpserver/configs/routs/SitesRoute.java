2dea9add9a6f063abd3296190a1ab35d68c9c047
Pawday <pawday@mail.ru>
2019-07-06 22:30:06 +0300

    SitesRoute class: constuctor is implemented.

--- src/main/java/org/kondle/httpserver/configs/routs/SitesRoute.java
+++ src/main/java/org/kondle/httpserver/configs/routs/SitesRoute.java
@@ -1,9 +1,40 @@
 package org.kondle.httpserver.configs.routs;
 
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class SitesRoute
 {
+	public SitesRoute(JSONObject jsonRoute) throws IllegalArgumentException, FileNotFoundException
+    {
+		if (jsonRoute.has("SiteAliace"))
+			this.siteAliace = jsonRoute.getString("SiteAliace");
+		else throw new IllegalArgumentException("In the json object there is no parameter SiteAliace");
+
+        if (jsonRoute.has("BaseDirectory"))
+        {
+            this.baseDirectory = new File(jsonRoute.getString("BaseDirectory"));
+            if (!this.baseDirectory.isAbsolute())
+                throw new IllegalArgumentException("The base directory must be specified as an absolute path.");
+            if (this.baseDirectory.exists() && this.baseDirectory.isFile())
+                throw new IllegalArgumentException(this.baseDirectory.getAbsolutePath() + " " + this.baseDirectory.getName() + " is not a directory");
+        }
+        else throw new IllegalArgumentException("In the json object there is no parameter BaseDirectory");
+
+        if (jsonRoute.has("DomainName"))
+            this.domainName = jsonRoute.getString("DomainName");
+        else throw new IllegalArgumentException("In the json object there is no parameter DomainName");
+
+        if (jsonRoute.has("port"))
+            this.port = jsonRoute.getInt("port");
+        else throw new IllegalArgumentException("In the json object there is no parameter port");
+
+        if (jsonRoute.has("ssl"))
+            this.sslParameters = new SSLParameters((JSONObject) jsonRoute.get("ssl"));
+	}
+
     private String siteAliace;
     private File baseDirectory;
     private String domainName;
@@ -12,14 +43,53 @@ public class SitesRoute
     public SSLParameters sslParameters;
 
 
-
-
-    class SSLParameters
+    public class SSLParameters
     {
+
+        private SSLParameters(JSONObject object)
+                throws IllegalArgumentException,FileNotFoundException
+        {
+            if (object.has("use"))
+                this.use = object.getBoolean("use");
+            else
+                throw new IllegalArgumentException("In the json object there is no parameter ssl.use");
+
+            if (this.use)
+            {
+                if (object.has("PKCS12File"))
+                {
+                    this.pkcs12File = new File(object.getString("PKCS12File"));
+                    if (!this.pkcs12File.exists())
+                        throw new FileNotFoundException(this.pkcs12File.getAbsolutePath() + " " + this.pkcs12File.getName() + " not found.");
+                    if (!this.pkcs12File.isFile())
+                        throw new IllegalArgumentException(this.pkcs12File.getAbsolutePath() + " " + this.pkcs12File.getName() + " is not a file");
+                } else
+                    throw new IllegalArgumentException("In the json object there is no parameter ssl.PKCS12File");
+
+                if (object.has("PKCS12FilePassword"))
+                {
+                    this.pkcs12FilePassword = object.getString("PKCS12FilePassword");
+                    if (this.pkcs12FilePassword.equals(""))
+                    {
+                        this.hasPKCS12FilePassword = false;
+                        this.pkcs12FilePassword = null;
+                    } else
+                        this.hasPKCS12FilePassword = true;
+                } else this.hasPKCS12FilePassword = false;
+
+                if (object.has("RedirectFromPort"))
+                {
+                    this.redirectFromPort = object.getInt("RedirectFromPort");
+                } else this.doRedirect = false;
+            }
+        }
+
         private boolean use = false;
         private File pkcs12File;
+        private boolean hasPKCS12FilePassword;
         private String pkcs12FilePassword;
         private int redirectFromPort = 80;
+        private boolean doRedirect = true;
 
         public boolean isUse()
         {
@@ -55,6 +125,21 @@ public class SitesRoute
         private void setRedirectFromPort(int redirectFromPort)
         {
             this.redirectFromPort = redirectFromPort;
+        }
+
+        public boolean hasPKCS12FilePassword()
+        {
+            return hasPKCS12FilePassword;
+        }
+
+        public boolean isRedirect()
+        {
+            return doRedirect;
+        }
+
+        public void setDoRedirect(boolean doRedirect)
+        {
+            this.doRedirect = doRedirect;
         }
     }
 
